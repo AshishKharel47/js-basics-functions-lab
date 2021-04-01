03eaf3c2328e7b62a2b8f8694229a56094313a09
AshishKharel <ashishkharel58@gmail.com>
2021-03-31 23:48:49 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup){
+    let Hq = 42;
+    if (pickup >Hq) {
+        return (pickup - Hq);
+    };
+    if (pickup < Hq) {
+        return (Hq - pickup);
+    };
+    
+}
+function distanceFromHqInFeet(variable){
+let Hq= 42;
+ if(variable>Hq){
+     return((variable-Hq)*264);
+ };
+     if (variable < Hq) {
+         return ((Hq-variable)*264);
+
+     };
+ 
+}
+function distanceTravelledInFeet(start,end){
+ if(start>end){
+     return((start-end)*264);
+ };
+     if (start < end) {
+         return ((end-start)*264);
+
+     };
+
+}
+function calculatesFarePrice(start, destination){
+   const distance = distanceTravelledInFeet(start,destination);
+   
+        if (distance <= 400){
+            return 0;
+          }
+          if (distance>400 && distance<=2000){
+            return (distance-400)*0.02;
+          }
+          if (distance>2000&& distance<=2500){
+            return 25;
+          }
+          if (distance>2500){
+              return 'cannot travel that far';
+          }
+        }
+
+
+
